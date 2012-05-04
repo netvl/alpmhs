a6f316679e63f8deda9dfc551c699e16622745b4
Vladimir Matveev <dpx.infinity@gmail.com>
2012-05-04 20:21:13 +0400

    Started high-level bindings for ALPM List.

--- lib/Distribution/ArchLinux/Libalpm/Raw/List.hsc
+++ lib/Distribution/ArchLinux/Libalpm/Raw/List.hsc
@@ -1,7 +1,10 @@
 #include <bindings.dsl.h>
-#include <alpm.h>
+#include <alpm_list.h>
 
 module Distribution.ArchLinux.Libalpm.Raw.List where
 #strict_import
 
+import Distribution.ArchLinux.Libalpm.Raw.Types
 
+#ccall alpm_list_add , Ptr <alpm_list_t> -> Ptr () -> IO (Ptr <alpm_list_t>)
+#ccall alpm_list_free , Ptr <alpm_list_t> -> IO ()
--- lib/Distribution/ArchLinux/Libalpm/Wrapper/List.hs
+++ lib/Distribution/ArchLinux/Libalpm/Wrapper/List.hs
@@ -2,10 +2,21 @@ module Distribution.ArchLinux.Libalpm.Wrapper.List where
 
 import Foreign.Ptr
 import Foreign.Storable
+import Foreign.C.String
 
 import Distribution.ArchLinux.Libalpm.Raw
 
 data AlpmList a = AlpmList (Ptr C'alpm_list_t)
 
 fromList :: (Storable a) => [a] -> AlpmList a
-fromList = undefined
+fromList []     = AlpmList nullPtr
+
+fromListStr :: [String] -> IO (AlpmList String)
+fromListStr [] = return $ AlpmList nullPtr
+fromListStr ss = go nullPtr ss
+    where
+        go list [] = return $ AlpmList list
+        go list (x:xs) = do 
+            cx <- newCString x
+            nlist <- c'alpm_list_add list (castPtr cx)
+            go nlist xs
